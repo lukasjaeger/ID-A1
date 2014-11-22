9be20a68be2d798b29d99d3ab5381319e51089f4
Lukas Jäger <crimson_sunrise@gmx.de>
2014-11-22 21:04:09 +0100

    Lossless recording capability added

--- src/de/tu_darmstadt/id_a1/AppModesActivity.java
+++ src/de/tu_darmstadt/id_a1/AppModesActivity.java
@@ -1,9 +1,15 @@
 package de.tu_darmstadt.id_a1;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -16,9 +22,7 @@ import android.widget.Toast;
 public class AppModesActivity extends Activity {
 	Button startBroadcast;
 	Button recordDummyButton;
-	int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we
-									// use only 1024
-	int BytesPerElement = 2; // 2 bytes in 16bit format
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -66,32 +70,117 @@ public class AppModesActivity extends Activity {
 	}
 
 	private void recordFile() {
+		// Preliminary settings ordered for easy changeability
+		final int timeInSeconds = 5;							//Duration of recording
+		final int audioSource = MediaRecorder.AudioSource.MIC;	//Audio source: Microphone
+		final int sampleRate = 44100;							//Default samplerate that is supported on all Android devices
+		final int channelConfig = AudioFormat.CHANNEL_IN_MONO;	//Mono...let's not make things too complicated
+		final int audioFormat = AudioFormat.ENCODING_PCM_16BIT;	//16 bit PCM-encoding
+		final int byteRate = sampleRate * 2;					//One sample has 16 bits
+		final int bufferSize = byteRate * timeInSeconds;		
 		String absolutePath = Environment.getExternalStoragePublicDirectory(
 				Environment.DIRECTORY_MUSIC).getAbsolutePath()
 				+ "/";
-		String fileName = "testfile.3gp";
-		final MediaRecorder mr = new MediaRecorder();
-		mr.setAudioSource(MediaRecorder.AudioSource.MIC);
-		mr.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-		mr.setOutputFile(absolutePath + fileName);
-		mr.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-		try {
-			mr.prepare();
-			mr.start();
-			Toast.makeText(AppModesActivity.this, "Recording started",
-					Toast.LENGTH_SHORT).show();
-			Thread.sleep(7000);
-			mr.stop();
-			Toast.makeText(AppModesActivity.this, "Recording stopped",
-					Toast.LENGTH_SHORT).show();
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		mr.release();
-	}
+		String fileName = "testfile.wav";
+		AudioRecord ar = new AudioRecord(audioSource, sampleRate,
+				channelConfig, audioFormat, bufferSize);
+		byte[] buffer = new byte[bufferSize];
 
+		// Starting time is taken to measure the record duration;
+		long startingTime = System.currentTimeMillis();
+
+		// Samples are recorded
+		ar.startRecording();
+		while (System.currentTimeMillis() - startingTime < timeInSeconds * 1000) {
+			ar.read(buffer, 0, bufferSize);
+		}
+		ar.stop();
+		Toast.makeText(AppModesActivity.this, "Recording stopped",
+				Toast.LENGTH_SHORT).show();
+		
+		//Calculating some values for the wave-file
+		int fileSize = bufferSize;
+		int totalSize = fileSize + 44;
+
+		// Delete the old recording and creating a new file
+		File f = new File(absolutePath + fileName);
+		if (f.exists())
+			f.delete();
+		try {
+			f.createNewFile();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			Toast.makeText(AppModesActivity.this, "Could not create file",
+					Toast.LENGTH_SHORT).show();
+		}
+		try {
+			DataOutputStream outStream = new DataOutputStream(
+					new FileOutputStream(absolutePath + fileName));
+			// Creating the RIFF-header
+			byte[] header = new byte[44];
+
+			header[0] = 'R'; // RIFF/WAVE header
+			header[1] = 'I';
+			header[2] = 'F';
+			header[3] = 'F';
+			header[4] = (byte) (totalSize - 8 & 0xff);
+			header[5] = (byte) ((totalSize - 8 >> 8) & 0xff);
+			header[6] = (byte) ((totalSize - 8 >> 16) & 0xff);
+			header[7] = (byte) ((totalSize - 8 >> 24) & 0xff);
+			header[8] = 'W';
+			header[9] = 'A';
+			header[10] = 'V';
+			header[11] = 'E';
+			header[12] = 'f'; // 'fmt ' chunk
+			header[13] = 'm';
+			header[14] = 't';
+			header[15] = ' ';
+			header[16] = 16; // 4 bytes: size of 'fmt ' chunk
+			header[17] = 0;
+			header[18] = 0;
+			header[19] = 0;
+			header[20] = 1; // format = 1
+			header[21] = 0;
+			header[22] = (byte) 1;
+			header[23] = 0;
+			header[24] = (byte) (sampleRate & 0xff);
+			header[25] = (byte) ((sampleRate >> 8) & 0xff);
+			header[26] = (byte) ((sampleRate >> 16) & 0xff);
+			header[27] = (byte) ((sampleRate >> 24) & 0xff);
+			header[28] = (byte) (byteRate & 0xff);
+			header[29] = (byte) ((byteRate >> 8) & 0xff);
+			header[30] = (byte) ((byteRate >> 16) & 0xff);
+			header[31] = (byte) ((byteRate >> 24) & 0xff);
+			header[32] = (byte) (2 * 16 / 8); // block align
+			header[33] = 0;
+			header[34] = 16; // bits per sample
+			header[35] = 0;
+			header[36] = 'd';
+			header[37] = 'a';
+			header[38] = 't';
+			header[39] = 'a';
+			header[40] = (byte) (fileSize & 0xff);
+			header[41] = (byte) ((fileSize >> 8) & 0xff);
+			header[42] = (byte) ((fileSize >> 16) & 0xff);
+			header[43] = (byte) ((fileSize >> 24) & 0xff);
+
+			outStream.write(header, 0, 44);
+
+			// Buffer written to output file
+			outStream.write(buffer);
+			outStream.flush();
+			outStream.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Toast.makeText(AppModesActivity.this, "FileNotFound",
+					Toast.LENGTH_SHORT).show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Toast.makeText(AppModesActivity.this, "IOException",
+					Toast.LENGTH_SHORT).show();
+		}
+	}
 }
--- src/de/tu_darmstadt/id_a1/AudioProbMainActivity.java
+++ src/de/tu_darmstadt/id_a1/AudioProbMainActivity.java
@@ -1,13 +1,12 @@
 package de.tu_darmstadt.id_a1;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -19,30 +18,7 @@ public class AudioProbMainActivity extends Activity {
 	private Button startButton;
 	
 	private WifiManager wifiManager;
-	
-	private BroadcastReceiver wifiStateChangeListener = new BroadcastReceiver() {
 
-		public void onReceive(Context context, Intent intent) {
-
-			updateWifiSettings();
-		}
-	};
-	
-	private BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
-
-		public void onReceive(Context context, Intent intent) {
-			
-			/**
-			 * If the WiFi is enabled then next activity will start
-			 */
-			if (wifiSwitchButton.isChecked()) {
-				Intent nextIntent = new Intent(AudioProbMainActivity.this, AppModesActivity.class);
-				startActivity(nextIntent);
-			}
-		}
-	};
-	
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -55,26 +31,41 @@ public class AudioProbMainActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				if (wifiSwitchButton.isChecked()) //Wifi switch button is checked, start listening on wifi
-				{
+				if (wifiSwitchButton.isChecked()) { //Wifi switch button is checked, start listening on wifi
 					Intent nextIntent = new Intent(AudioProbMainActivity.this, AppModesActivity.class);
 					startActivity(nextIntent);
 				}
 				else //Nothing chosen. Print a message that tells the user to decide
-				{
 					Toast.makeText(AudioProbMainActivity.this, "Please activate Wifi!", Toast.LENGTH_SHORT).show();
-				}
 				
 			}
 		});
-		updateWifiSettings();
+		
+		wifiSwitchButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String wifiIsOnText = getString(R.string.switchWiFiOn_text);
+				String wifiIsOffText = getString(R.string.switchWiFiOff_text);
+
+				if (wifiSwitchButton.isChecked()) {
+					//Toast.makeText(getBaseContext(), wifiIsOnText, Toast.LENGTH_SHORT).show();
+					Intent nextIntent = new Intent(getBaseContext(), AppModesActivity.class);
+					startActivity(nextIntent);
+				}
+				else
+					Toast.makeText(getBaseContext(), wifiIsOffText, Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		
 	}
 
 	protected void onResume() {
 
 		super.onResume();
 
-		registerReceiver(wifiStateChangeListener, new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
+		//registerReceiver(wifiStateChangeListener, new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
 	}
 
 
@@ -82,25 +73,6 @@ public class AudioProbMainActivity extends Activity {
 
 		super.onPause();
 
-		unregisterReceiver(wifiStateChangeListener);
-	}
-	
-	private void updateWifiSettings() {
-
-		wifiSwitchButton.setChecked(wifiManager.isWifiEnabled());
-
-		String wifiIsOnText = getString(R.string.switchWiFiOn_text);
-		String wifiIsOffText = getString(R.string.switchWiFiOff_text);
-
-		if (wifiSwitchButton.isChecked())
-			Toast.makeText(AudioProbMainActivity.this, wifiIsOnText, Toast.LENGTH_SHORT).show();
-		else
-			Toast.makeText(AudioProbMainActivity.this, wifiIsOffText, Toast.LENGTH_SHORT).show();
-	}
-	
-	public void switchWiFi(View view) {
-
-		wifiManager.setWifiEnabled(wifiSwitchButton.isChecked());
-
+		//unregisterReceiver(wifiStateChangeListener);
 	}
 }
